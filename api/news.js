35de34fd7a3e301c63a143a9e3b151f13ea92e0c
sophia <scastellarin95@gmail.co>
2014-12-06 13:55:42 -0800

    make it use the db.js module

--- api/news.js
+++ api/news.js
@@ -2,22 +2,18 @@
 
 var mongojs = require('mongojs');
 var util = require('util');
-
-var dbuser = 'user';
-var dbpassword = 'tuba';
-var uri = util.format(
-        'mongodb://%s:%s@ds061360.mongolab.com:61360/conaps',
-        dbuser,
-        dbpassword
-);
-var db = mongojs(uri);
+var db = require('../db/db.js');
 var news = db.collection('news');
 
-exports.getData = function(startDate, endDate, loc){
-    // TODO: implementation
+exports.getData = function(query, cb){
+    news.find(function(err, docs){
+        cb(docs);
+    });
 };
 
 exports.getNews = function(startDate, endDate, loc){
-    // TODO: implementation
+     news.find(function(err, docs){
+        cb(docs);
+    });
 };
 
