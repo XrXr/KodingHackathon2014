aa9011dfd82d6c5012aa4e30ae19a1fb3fa02bae
sophia <scastellarin95@gmail.co>
2014-12-06 13:07:24 -0800

    can connect and read from db

--- api/news.js
+++ api/news.js
@@ -1,4 +1,23 @@
 // module that gets news data from the db
-// TODO: (1) connect to db, 
-//      (2) query for something given time and location, 
-//      (3) return json
+
+var mongojs = require('mongojs');
+var util = require('util');
+
+var dbuser = 'user';
+var dbpassword = 'tuba';
+var uri = util.format(
+        'mongodb://%s:%s@ds061360.mongolab.com:61360/conaps',
+        dbuser,
+        dbpassword
+);
+var db = mongojs(uri);
+var news = db.collection('news');
+
+exports.getData = function(startDate, endDate, loc){
+    // TODO: implementation
+};
+
+exports.getNews = function(startDate, endDate, loc){
+    // TODO: implementation
+};
+
