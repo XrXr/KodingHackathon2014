d56274a1e147d2a945b4d6f32c671e59aeed11a1
sophia <scastellarin95@gmail.co>
2014-12-06 13:49:56 -0800

    add db module

---
+++ db/db.js
@@ -0,0 +1,16 @@
+var mongojs = require('mongojs');
+var util = require('util');
+
+var dbuser = 'user';
+var dbpassword = 'tuba';
+var uri = util.format(
+        'mongodb://%s:%s@ds061360.mongolab.com:61360/conaps',
+        dbuser,
+        dbpassword
+);
+var db = mongojs(uri);
+
+exports.collection = function(dbColl) {
+    return db.collection(dbColl);
+};
+
