382386c8d85247ce552131fbb9fa4519b5547003
sophia <scastellarin95@gmail.co>
2014-12-06 16:48:22 -0800

    can return data from getData function

--- api/news.js
+++ api/news.js
@@ -1,13 +1,27 @@
 // module that gets news data from the db
 
-var mongojs = require('mongojs');
 var util = require('util');
 var db = require('../db/db.js');
 var news = db.collection('news');
 
 exports.getData = function(query, cb){
-    news.find(function(err, docs){
-        cb(docs);
+    var d = db.database(['news', 'agg']);
+    var mapper = function(){
+        emit(this.loc, 1);
+    };
+    var reducer = function(_id, c){
+        return Array.sum(c);
+    };
+    d.news.mapReduce(mapper, reducer, {out:'agg'});
+    d.agg.find(function(err, docs){
+        var data = docs.map(function(d){
+            var td = {};
+            td['lat'] = d["_id"]["lat"];
+            td['lng'] = d["_id"]["long"];
+            td['value'] = d["value"];
+            return td;
+        });
+        cb(data);
     });
 };
 
