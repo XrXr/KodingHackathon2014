371feae0cec057fbec0c327d13421e311e8ebdec
sophia <scastellarin95@gmail.co>
2014-12-06 19:08:21 -0800

    add js deps

---
+++ public/js/app.js
@@ -0,0 +1,25 @@
+var latlng = new google.maps.LatLng(25.6, -80.3);
+var opts = {
+    center: latlng
+};
+
+map = new google.maps.Map(document.getElementById("map-canvas"), opts);
+
+heatmap = new HeatmapOverlay(map,
+        {
+            "radius": 2,
+            "maxOpacity": 1,
+            "scaleRadius": true,
+            "useLocalExtrema": true,
+            latField: 'lat',
+            lngField: 'lng',
+            valueField: 'count'
+        }
+);
+
+var testData = {
+      max: 8,
+        data: [{lat: 24.6408, lng:46.7728, count: 3},{lat: 50.75, lng:-1.55, count: 1}]
+};
+
+heatmap.setData(testData);
