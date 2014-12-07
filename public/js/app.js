29e564f331fbd0bdc6556dc53fdafb1af64156eb
sophia <scastellarin95@gmail.co>
2014-12-07 14:38:00 -0800

    add link for title and source

--- public/js/app.js
+++ public/js/app.js
@@ -8,7 +8,7 @@ map = new google.maps.Map(document.getElementById("map-canvas"), opts);
 
 heatmap = new HeatmapOverlay(map,
         {
-            "radius": 2,
+            "radius": 4,
             "maxOpacity": 1,
             "scaleRadius": true,
             "useLocalExtrema": true,
@@ -28,14 +28,16 @@ $.getJSON("/api/newsdata", function(d){
 $.getJSON("/api/newsstories", function(d){
     var genHTML = "<h3>Stories</h3>";
     d.forEach(function(data) {
- 
+
         $.getJSON("https://maps.googleapis.com/maps/api/geocode/json?latlng=" + data['loc']['lat'] + "," + data['loc']['long'], function(loc){
             var r = loc["results"][0]["address_components"];
             var locData = r[r.length-1]['long_name'];
         
             d = {
                 "title": data["title"],
-                "loc": locData
+                "loc": locData,
+                "link": data["link"],
+                "source": data["source"]
             };
             genHTML = genHTML + generateInfo(d);
 
@@ -45,32 +47,12 @@ $.getJSON("/api/newsstories", function(d){
     });
 });
 
-function addStories(d){
-    var genHTML = "<h3>Stories</h3>";
-    d.forEach(function(data) {
- 
-        $.getJSON("https://maps.googleapis.com/maps/api/geocode/json?latlng=" + data['loc']['lat'] + "," + data['loc']['long'], function(loc){
-            var r = loc["results"][0]["address_components"];
-            var locData = r[r.length-1]['long_name'];
-        
-            d = {
-                "title": data["title"],
-                "loc": locData
-            };
-            genHTML = genHTML + generateInfo(d);
-
-            $(".info")[0].innerHTML = genHTML; 
-        });
-
-    });
-
-
-}
-
 function generateInfo(data){
     return "<div class='story'>" +
-        "<div><b>" + data['title'] + "</b></div>" +
-        "<span class='small'>location</span> <span>" + data["loc"]  + 
-        "</span>"+
+        "<div><a href=" + data['link'] + ">" + data['title'] + "</a></div>" +
+        "<div><span class='small'>location: </span><span>" + data["loc"]  + 
+        "</span></div>"+
+        "<div><span class='small'>source: </span><span>"+ data["source"] +
+        "</span></div>"+
         "</div>";
 }
